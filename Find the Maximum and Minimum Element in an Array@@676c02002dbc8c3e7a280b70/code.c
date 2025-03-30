e01bf3da196caff562411044f2911d47290d52af
Aneesh-08 <aneeshranhotra@gmail.com>
2025-03-30 13:33:09 +0530

    Solution to Find the Maximum and Minimum Element in an Array

---
+++ Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
@@ -0,0 +1,26 @@
+#include <iostream>  // Include input-output library
+using namespace std;  // Use standard namespace
+
+int main() {  // Main function
+    
+    int x;
+    cin>>x;
+    int arr[x];
+    for(int i = 0;i<x;i++){
+        cin>>arr[i];
+    }
+
+    for(int i = 0 ; i < x;i++){
+        for(int j = 0 ; j < x;j++){
+            if(arr[i]>arr[j]){
+                swap(arr[i],arr[j]);
+            }
+        }
+    }
+    cout<<arr[0]<<" "<<arr[x-1];
+
+
+
+
+    return 0;  // Return 0 indicates successful execution
+}
